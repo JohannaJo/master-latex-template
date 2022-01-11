9e138d69e1f099d6b7e3d70c23499854fc044a56
JohannaJo <Lisa.josang@student.uib.no>
2022-01-11 09:34:07 +0100

    extend_and_mine implemented

--- code/kb_extension.py
+++ code/kb_extension.py
@@ -2,10 +2,8 @@ import numpy as np
 import pandas as pd
 from candidate_generation import generate_candidate_triples
 from candidate_ranking import rank_candidate_triples, get_candidates_above_rank
-from rule_generation import rule_set_generation
+from rule_mining import rule_mining
 
-# for testing
-from ampligraph.latent_features import restore_model
 
 
 family_subset = np.loadtxt("family_subset_test.txt", dtype = 'object')
@@ -14,6 +12,7 @@ family_relations = ["child", "sibling", "mother", "father", "relative", "spouse"
 def extend_kb(original_kb, model, entity_selection_process, candidate_admittance_criteria):
     # entities = get_entities(entitie_selection_process) TODO
     candidates, entities = generate_candidate_triples(original_kb, entities=None, max_entities=10, relations=family_relations, savefile_name=None)
+    print("Candidate generation complete")
     ranked_candidate_triples = rank_candidate_triples(model, candidates, original_kb, entities, savefile_name=None)
     # admitted_candidates = filter_candidates(candidate_admittance_criteria) TODO
     admitted_candidates = get_candidates_above_rank(ranked_candidate_triples, rank_cutoff = 3)
@@ -22,13 +21,28 @@ def extend_kb(original_kb, model, entity_selection_process, candidate_admittance
     expanded_kb = np.concatenate([original_kb, admitted_candidates_ndarray])
     return expanded_kb
 
-#def mine_rule_sets(original_kb, model, entity_selection_process, candidate_admittance_criteria):
-    #rule_set_A = 
+def extend_and_mine(original_kb, model, entity_selection_process, candidate_admittance_criteria):
+    """
+    Takes a knowledge base and extends it by adding new triples that pass some admittance criteria. Therafter rules are mined from the extended knowledge base.
+    
+    :param........TODO
+    :return rules: Pandas dataframe containing the mined rules and metrics.
+    """
+    extended_kb = extend_kb(original_kb, model, entity_selection_process, candidate_admittance_criteria)
+    rules = rule_mining(kb=original_kb, save_raw_mining_output=False, save_mined_rules=False)
+    return rules, extended_kb
 
+"""
 # testing
+from ampligraph.latent_features import restore_model
+
 model = restore_model('./Wikidata_family_subset_100_epocs.pkl')
 candidate_triples = np.loadtxt("delete.txt", dtype = 'object')
 entities = np.loadtxt("delete_entities.txt", dtype = 'object')
 print(family_subset.shape)
 print("\n")
-print(extend_kb(family_subset, model, entity_selection_process=None, candidate_admittance_criteria=None).shape)
+
+r, ex_kb = extend_and_mine(family_subset, model, entity_selection_process=None, candidate_admittance_criteria=None)
+print(r)
+print(ex_kb.shape)
+"""
