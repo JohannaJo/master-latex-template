f7d90cf500172f7f7a5ff638576e0cad83ccdc00
JohannaJo <Lisa.josang@student.uib.no>
2022-01-11 09:41:46 +0100

    Added comments

--- code/kb_extension.py
+++ code/kb_extension.py
@@ -4,12 +4,19 @@ from candidate_generation import generate_candidate_triples
 from candidate_ranking import rank_candidate_triples, get_candidates_above_rank
 from rule_mining import rule_mining
 
-
-
-family_subset = np.loadtxt("family_subset_test.txt", dtype = 'object')
+# used as default parameter
 family_relations = ["child", "sibling", "mother", "father", "relative", "spouse"]
 
 def extend_kb(original_kb, model, entity_selection_process, candidate_admittance_criteria):
+       """
+    Takes a knowledge base and extends it by adding new triples that pass some admittance criteria.
+    
+    :param original_kb: numpy ndarray representing the original knowledge base.
+    :param model: knowledge graph embedding trained on the original knowledge base.
+    :param entity_selection_process: parameter for how entities are generated.
+    :param candidate_admittance_criteria: parameter for filtering out triples not deemed good enough for addition to the kb.
+    :return expanded_kb: numpy ndarray containing the original kb with additional new triples, ie an extended version of the original kb.
+    """
     # entities = get_entities(entitie_selection_process) TODO
     candidates, entities = generate_candidate_triples(original_kb, entities=None, max_entities=10, relations=family_relations, savefile_name=None)
     print("Candidate generation complete")
@@ -25,7 +32,10 @@ def extend_and_mine(original_kb, model, entity_selection_process, candidate_admi
     """
     Takes a knowledge base and extends it by adding new triples that pass some admittance criteria. Therafter rules are mined from the extended knowledge base.
     
-    :param........TODO
+    :param original_kb: numpy ndarray representing the original knowledge base.
+    :param model: knowledge graph embedding trained on the original knowledge base.
+    :param entity_selection_process: parameter for how entities are generated.
+    :param candidate_admittance_criteria: parameter for filtering out triples not deemed good enough for addition to the kb.
     :return rules: Pandas dataframe containing the mined rules and metrics.
     """
     extended_kb = extend_kb(original_kb, model, entity_selection_process, candidate_admittance_criteria)
@@ -36,6 +46,7 @@ def extend_and_mine(original_kb, model, entity_selection_process, candidate_admi
 # testing
 from ampligraph.latent_features import restore_model
 
+family_subset = np.loadtxt("family_subset_test.txt", dtype = 'object')
 model = restore_model('./Wikidata_family_subset_100_epocs.pkl')
 candidate_triples = np.loadtxt("delete.txt", dtype = 'object')
 entities = np.loadtxt("delete_entities.txt", dtype = 'object')
