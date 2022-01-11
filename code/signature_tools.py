599bb95ea981b15347b92ca24a414cd426f752fd
JohannaJo <Lisa.josang@student.uib.no>
2022-01-11 10:59:37 +0100

    cleanup + random selection
    Added functions for random selection of entities and predicates.

--- code/signature_tools.py
+++ code/signature_tools.py
@@ -2,6 +2,7 @@ import numpy as np
 
 """
 The file contains helper functions for selecting entities and triples in a knowledge base by their frequencies or signature.
+It assumes a dataset in the format of a 2d numpy array with three columns, respectively representing the object, predicate and subject of each triple.
 """
 
 def get_object_frequencies(dataset):
@@ -77,21 +78,20 @@ def subset_by_strict_signature(dataset, objects, predicates, targets):
     return dataset[np.isin(dataset[:,0], objects) & np.isin(dataset[:,1],predicates) & np.isin(dataset[:,2], targets)]
 
 
-
 def subset_by_frequency(dataset, min_predicate_freq, min_object_target_freq):
     predicate_freq = get_predicate_frequencies(dataset)
     relevant_predicates = predicate_freq[np.where(predicate_freq[:,1] >= min_predicate_freq)]
     relevant_predicates = relevant_predicates[:,0] # extract list of relevant predicates
-    yago_filtered = dataset[np.isin(dataset[:,1], relevant_predicates)]
-    object_freq = get_object_frequencies(yago_filtered)
-    objects_and_targets = np.concatenate([yago_filtered[:, 0], yago_filtered[:, 2]])
+    dataset_filtered = dataset[np.isin(dataset[:,1], relevant_predicates)]
+    object_freq = get_object_frequencies(dataset_filtered)
+    objects_and_targets = np.concatenate([dataset_filtered[:, 0], dataset_filtered[:, 2]])
     unique, counts = np.unique(objects_and_targets, return_counts=True)
     object_target_freq = np.asarray((unique, counts)).T
     relevant_obj_target = object_target_freq[np.where(object_target_freq[:,1] >= min_object_target_freq)]
     relevant_obj_target = relevant_obj_target[:,0]
-    yago_filtered = yago_filtered[np.isin(yago_filtered[:,0], relevant_obj_target) | np.isin(yago_filtered[:,2], relevant_obj_target)]
+    dataset_filtered = dataset_filtered[np.isin(dataset_filtered[:,0], relevant_obj_target) | np.isin(dataset_filtered[:,2], relevant_obj_target)]
 
-    return yago_filtered
+    return dataset_filtered
 
 
 def most_frequent_objects(dataset, n = 10):
@@ -103,41 +103,56 @@ def most_frequent_objects(dataset, n = 10):
     """
     frequencies = get_object_frequencies(dataset)
     sorted_frequencies = frequencies[frequencies[:, 1].argsort()]
-    sorted_frequencies = np.flip(sorted_frequencies)
+    sorted_frequencies = np.flip(sorted_frequencies, axis=0)
     
     return sorted_frequencies[:n]
+
 
 def least_frequent_objects(dataset, n = 10):
     """
     Finds the least frequent objects in a dataset of triplets, and returns the n least frequent with their corresponding frequencies.
     
-    :param dataset: set of triplets
+    :param dataset: set of triplets.
     :param n: number of least frequent objects to return.
     """
     frequencies = get_object_frequencies(dataset)
     sorted_frequencies = frequencies[frequencies[:, 1].argsort()]
     
     return sorted_frequencies[:n]
+
+
+def random_objects(dataset, n = 10):
+    """
+    Returns n unique random objects from the dataset.
+    
+    :param dataset: set of triplets.
+    :param n: number of random objects to return.
+    """
+    objects = dataset[:,0]
+    unique_objects = np.unique(objects)
+    random_subset_of_objects = np.random.choice(unique_objects, size=n)
+    return random_subset_of_objects
     
     
 def most_frequent_predicates(dataset, n = 10):
     """
     Finds the most frequent predicates in a dataset of triplets, and returns the n most frequent with their corresponding frequencies.
     
-    :param dataset: set of triplets
+    :param dataset: set of triplets.
     :param n: number of most frequent predicates to return.
     """
     frequencies = get_predicate_frequencies(dataset)
     sorted_frequencies = frequencies[frequencies[:, 1].argsort()]
-    sorted_frequencies = np.flip(sorted_frequencies)
+    sorted_frequencies = np.flip(sorted_frequencies, axis=0)
     
     return sorted_frequencies[:n]
+
 
 def least_frequent_predicates(dataset, n = 10):
     """
     Finds the least frequent predicates in a dataset of triplets, and returns the n least frequent with their corresponding frequencies.
     
-    :param dataset: set of triplets
+    :param dataset: set of triplets.
     :param n: number of least frequent predicates to return.
     """
     frequencies = get_predicate_frequencies(dataset)
@@ -146,27 +161,54 @@ def least_frequent_predicates(dataset, n = 10):
     return sorted_frequencies[:n]
 
 
+def random_predicates(dataset, n = 10):
+    """
+    Returns n unique random predicates from the dataset.
+    
+    :param dataset: set of triplets.
+    :param n: number of random predicates to return.
+    """
+    predicates = dataset[:,1]
+    unique_predicates = np.unique(predicates)
+    random_subset_of_predicates = np.random.choice(unique_predicates, size=n)
+    return random_subset_of_predicates
+
+
 def most_frequent_targets(dataset, n = 10):
     """
     Finds the most frequent targets in a dataset of triplets, and returns the n most frequent with their corresponding frequencies.
     
-    :param dataset: set of triplets
+    :param dataset: set of triplets.
     :param n: number of most frequent targets to return.
     """
     frequencies = get_target_frequencies(dataset)
     sorted_frequencies = frequencies[frequencies[:, 1].argsort()]
-    sorted_frequencies = np.flip(sorted_frequencies)
+    sorted_frequencies = np.flip(sorted_frequencies, axis=0)
     
     return sorted_frequencies[:n]
+
 
 def least_frequent_targets(dataset, n = 10):
     """
     Finds the most frequent targets in a dataset of triplets, and returns the n least frequent with their corresponding frequencies.
     
-    :param dataset: set of triplets
+    :param dataset: set of triplets.
     :param n: number of least frequent targets to return.
     """
     frequencies = get_target_frequencies(dataset)
     sorted_frequencies = frequencies[frequencies[:, 1].argsort()]
     
     return sorted_frequencies[:n]
+
+
+def random_targets(dataset, n = 10):
+    """
+    Returns n unique random targets from the dataset.
+    
+    :param dataset: set of triplets.
+    :param n: number of random objects to return.
+    """
+    targets = dataset[:,2]
+    unique_targets = np.unique(targets)
+    random_subset_of_targets = np.random.choice(unique_targets, size=n)
+    return random_subset_of_targets
