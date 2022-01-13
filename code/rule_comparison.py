b27ae87c9398f5b38234697df119412f38f91a26
JohannaJo <Lisa.josang@student.uib.no>
2022-01-13 10:42:56 +0100

    Display rule comparison

--- code/rule_comparison.py
+++ code/rule_comparison.py
@@ -4,11 +4,13 @@ import seaborn as sns
 
 
 def combine_rules(rule_set_A, rule_set_B):
-    return pd.merge(rule_set_A, rule_set_B, how='outer', on=['Rule'])
+    merged_rules = pd.merge(rule_set_A, rule_set_B, how='outer', on=['Rule'])
+    return merged_rules
 
 
 def get_common_rules(rule_set_A, rule_set_B):
-    return pd.merge(rule_set_A, rule_set_B, how='inner', on=['Rule', "Functional_variable"])
+    commom_rules = pd.merge(rule_set_A, rule_set_B, how='inner', on=['Rule', 'Functional variable'])
+    return commom_rules
     
     
 def get_unique_rules(rule_set_A, rule_set_B):
@@ -32,4 +34,34 @@ def plot_pie_chart(rule_set_A, rule_set_B):
     plt.pie(data, labels = labels, colors = colors, autopct='%.0f%%')
     plt.title("Distribution of rules mined")
     plt.show()
+    
 
+def print_comparison_details(rule_set_A, rule_set_B, index=0):
+    common = get_common_rules(rule_set_A, rule_set_B)
+    unique_A, unique_B = get_unique_rules(rule_set_A, rule_set_B)
+    print("EXPANDED RULE SET ", index)
+    print("Unique original:", len(unique_A)," Unique expanded:", len(unique_B), " Common rules:", len(common))
+
+    
+def display_comparison(rule_set_A, rule_set_B, index=0):
+    common = get_common_rules(rule_set_A, rule_set_B)
+    unique_A, unique_B = get_unique_rules(rule_set_A, rule_set_B)
+    common = len(common)
+    unique_A = len(unique_A)
+    unique_B = len(unique_B)
+    
+    # print details
+    print("EXPANDED RULE SET ", index)
+    print("Unique original:", unique_A," Unique expanded:", unique_B, " Common rules:", common)
+    
+     # plot distribution of the mined rules
+    data = [common, unique_A, unique_B]
+    labels = ['Common for both mined sets', 'Unique to original', 'Unique to expanded']
+
+    #define Seaborn color palette to use
+    colors = sns.color_palette('pastel')[0:5]
+
+    #create pie chart
+    plt.pie(data, labels = labels, colors = colors, autopct='%.0f%%')
+    plt.title("Distribution of rules mined")
+    plt.show()
